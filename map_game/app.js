fe053f5db9a7481667ad6c5494fb0c00fcb3f42b
Paras Waghela <paraswaghela777@gmail.com>
2024-02-27 22:51:23 +0530

    map game started

---
+++ map_game/app.js
@@ -0,0 +1,40 @@
+const states=[
+    'Maharashtra','gujarat','rajasthan'
+];
+let allinputs=document.querySelectorAll("input[type='radio']");
+let userAns;
+let q;
+function load(){
+    q=states[Math.floor(Math.random()*states.length)]
+    console.log("Q ka value == ", q);
+    document.getElementById('qst').innerText=q;
+}
+
+
+// console.log(allinputs)
+
+    
+    document.getElementById('btn').addEventListener('click',function(){
+        allinputs.forEach(element => {
+            if(element.checked==true){
+                userAns=element.value
+                return;
+            }
+        });
+    
+        if(q.toUpperCase()===userAns.toUpperCase()){
+            document.getElementById('res').innerText="Sahi he";
+        }
+        else{
+            document.getElementById('res').innerText="galt he";
+        }
+        console.log(q);
+        console.log(userAns);
+    })
+
+    load();
+
+
+
+
+
